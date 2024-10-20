4b772b8bdd93e8d28d4c86e849498e4a7daf8ed1
Soham Singh <140815077+sohxm135@users.noreply.github.com>
2024-10-21 01:05:47 +0530

    Create Q1.java

---
+++ OOP Lab/Lab10/Q1.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String inp = sc.nextLine();
+        int n;
+        try{
+        n =  Integer.parseInt(inp);
+        System.out.println(n);
+        }
+        catch(NumberFormatException A){
+            System.out.println("Invalid input\n"+A);
+        }
+        System.out.println("After try catchh");
+    }
+}
